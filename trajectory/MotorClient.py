7c597306f6151d6dff1074e8064a4ecddbcd2383
paucarre <pau.carre@gmail.com>
2018-01-21 12:28:33 +0000

    code recatoring adding motor client class

---
+++ trajectory/MotorClient.py
@@ -0,0 +1,90 @@
+import random
+import os
+import re
+import threading
+import time
+import datetime
+import json
+import sys
+import socket
+
+class MotorClient():
+
+    def __init__(self, articulation_ips):
+        self.articulation_ips = articulation_ips
+
+    def send_socket(self, message, current_socket):
+        bytes_sent = 0
+        is_connected = True
+        while bytes_sent < len(message) and is_connected:
+            current_bytes_sent = current_socket.send(message[bytes_sent:])
+            is_connected = current_bytes_sent > 0
+            if is_connected:
+                bytes_sent = bytes_sent + current_bytes_sent
+            else:
+                print(f"Socket '{socket}' disconnected")
+        return is_connected
+
+    def receive_socket(self, current_socket):
+        received_bytes = []
+        is_connected = True
+        while is_connected:
+            byte_received = current_socket.recv(1)
+            received_bytes.insert(len(received_bytes), byte_received.decode("utf-8"))
+            is_connected = byte_received != b''
+        return ''.join(received_bytes)
+
+    def create_connection(self, ip):
+        try:
+            current_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            current_socket.connect((ip, 23))
+            return current_socket
+        except e:
+            print(f"Connection to IP {ip} was unsucccesful due to '{e}'")
+            return None
+
+    def disconnect(self, socket):
+        try:
+            socket.disconnect()
+            return True
+        except BaseException as e:
+            print(f"Error trying to disconnect due to '{e}'")
+            return False
+
+    def get_position(self, ip):
+        socket = create_connection(ip)
+        position_as_float = None
+        if(socket is not None):
+            successful = send_socket(bytearray(b'?'), socket)
+            if(successful):
+                raw_position = receive_socket(socket)
+                if(raw_position is not None):
+                    position_as_float = float(raw_position)
+                else:
+                    print(f"Problem receiving positon from articulation in IP {ip}")
+            else:
+                print(f"Problem sending positon request to articulation in IP {ip}")
+            disconnect(socket)
+        else:
+            print(f"Problem connecting to articulation with IP {ip}")
+        return position_as_float
+
+    def set_position_(self, position, ip):
+        socket = create_connection(ip)
+        position_as_float = None
+        if(socket is not None):
+            possition_message = f">{position}"
+            position_message_byte = bytearray()
+            position_message_byte.extend(map(ord, possition_message))
+            successful = send_socket(position_message_byte, socket)
+            if(successful):
+                return True
+            else:
+                return False
+
+    def get_articulation_positions(self):
+        articulation_positions = []
+        for articulation_ip in self.articulation_ips:
+            articulation_position = get_position(articulation_ip)
+            articulation_positions.insert(len(articulation_positions), articulation_position)
+        return articulation_positions
--- trajectory/TrajectoryServer.py
+++ trajectory/TrajectoryServer.py
@@ -12,9 +12,6 @@ import json
 import sys
 import socket
 
-worker_ids = None
-worker_index = None
-worker_index_update_time = None
 app = Flask(__name__)
 app.config.from_envvar('ROBOT_TRAJECTORY_SERVER_SETTINGS')
 
@@ -28,76 +25,21 @@ def get_articulation_ips():
 
 articulation_ips = get_articulation_ips()
 
-def send_socket(message, current_socket):
-    bytes_sent = 0
-    is_connected = True
-    while bytes_sent < len(message) and is_connected:
-        current_bytes_sent = current_socket.send(message[bytes_sent:])
-        is_connected = current_bytes_sent > 0
-        if is_connected:
-            bytes_sent = bytes_sent + current_bytes_sent
-        else:
-            print(f"Socket '{socket}' disconnected")
-    return is_connected
-
-def receive_socket(current_socket):
-    received_bytes = []
-    is_connected = True
-    while is_connected:
-        byte_received = current_socket.recv(1)
-        received_bytes.insert(len(received_bytes), byte_received.decode("utf-8"))
-        print(f"****{byte_received}****")
-        is_connected = byte_received != b''
-    return ''.join(received_bytes)
-
-def create_connection(ip):
-    try:
-        current_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        current_socket.connect((ip, 23))
-        return current_socket
-    except e:
-        print(f"Connection to IP {ip} was unsucccesful due to '{e}'")
-        return None
-
-def disconnect(socket):
-    try:
-        socket.disconnect()
-        return True
-    except BaseException as e:
-        print(f"Error trying to disconnect due to '{e}'")
-        return False
-
-def get_current_position(ip):
-    socket = create_connection(ip)
-    position_as_float = None
-    if(socket is not None):
-        successful = send_socket(bytearray(b'?'), socket)
-        if(successful):
-            raw_position = receive_socket(socket)
-            if(raw_position is not None):
-                #print(f"*******{raw_position}*******")
-                position_as_float = float(raw_position)
-            else:
-                print(f"Problem receiving positon from articulation in IP {ip}")
-        else:
-            print(f"Problem sending positon request to articulation in IP {ip}")
-        disconnect(socket)
-    else:
-        print(f"Problem connecting to articulation with IP {ip}")
-    return position_as_float
-
-def get_articulation_positions():
-    articulation_positions = []
-    for articulation_ip in articulation_ips:
-        articulation_position = get_current_position(articulation_ip)
-        articulation_positions.insert(len(articulation_positions), articulation_position)
-    return articulation_positions
-
 @app.route('/positions', methods=['GET'])
 def positions_endpoint():
-    articulation_positions = get_articulation_positions()
+    motor_client = MotorClient(articulation_ips)
+    articulation_positions = motor_client.get_articulation_positions()
     return Response(f"{articulation_positions}", mimetype='text/html', status=200)
 
-@app.route('/move_to', methods=['POST'])
-def move_to():
-    pass
+@app.route('/move/<articulation>', methods=['POST'])
+def move_to(articulation):
+    motor_client = MotorClient(articulation_ips)
+    position = request.args.get('position')
+    if position is None:
+        return Response("Bad Request: 'position' argument required", status=400)
+    try:
+        position_as_int = float(positon)
+    except BaseException as e:
+        return Response(f"Bad Request: 'position' with value {positon} is not a float", status=400)
+    print(content)
+    return(uuid)
